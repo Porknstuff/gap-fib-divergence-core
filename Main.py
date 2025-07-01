bd7144828d958be7941119e44652727d30b64c1a
Porknstuff <Porknstuff@users.noreply.github.com>
2025-07-01 17:29:45 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from core import run_trading_system
+import threading
+
+app = FastAPI()
+
+@app.get("/health", response_class=PlainTextResponse)
+def health():
+    return "OK"
+
+@app.on_event("startup")
+def start_trader():
+    threading.Thread(target=run_trading_system, daemon=True).start()
